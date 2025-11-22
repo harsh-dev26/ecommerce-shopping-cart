c9a242a7b9799e230f022473b5780bdf11a36a1c
harsh-dev26 <hs123619@gmail.com>
2025-11-22 10:29:13 +0530

    Fix API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://ecommerce-backend.onrender.com";
+const API_BASE_URL = "https://ecommerce-backend-fjm9.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
